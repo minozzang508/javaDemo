71c4e6a2f58f2512d427827bfd83eb48a75e1ab5
minozzang508 <minozzang508@naver.com>
2020-09-16 15:39:29 +0900

    wrong kt lint

--- src/main/java/fundamental/RxBusStatus.kt
+++ src/main/java/fundamental/RxBusStatus.kt
@@ -7,17 +7,17 @@ import io.reactivex.subjects.PublishSubject
  * Created by phkim on 2017-12-22.
  */
 class RxBusStatus{
-    companion object {
-        private var behaviorSubject : PublishSubject<Pair<Constant.BleStatus,String>>? = null
-        fun getSubject():PublishSubject<Pair<Constant.BleStatus,String>>?{
-            if(behaviorSubject ==null){
-                behaviorSubject =  PublishSubject.create<Pair<Constant.BleStatus,String>>()
-            }
-            return behaviorSubject
-        }
+companion object {
+private var behaviorSubject : PublishSubject<Pair<Constant.BleStatus,String>>? = null
+fun getSubject():PublishSubject<Pair<Constant.BleStatus,String>>?{
+if(behaviorSubject ==null){
+behaviorSubject =  PublishSubject.create<Pair<Constant.BleStatus,String>>()
+}
+return behaviorSubject
+}
 
-        fun release(){
-            behaviorSubject = null
-        }
-    }
+fun release(){
+behaviorSubject = null
+}
+}
 }
